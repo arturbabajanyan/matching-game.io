eaa8de74cb760ee677be8defcd4a7b37d1b6c299
arturbabajanyan <104034809+arturbabajanyan@users.noreply.github.com>
2023-12-27 15:49:57 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 function reloadThePage() {
     location.reload();
 }
+const regex = new RegExp('card-back');
+const storeTwoCards = {};
 
 let index = 0;
-
-
 let stopLoop = 20;
+let startGame = false;
+
 
 const cards = ["AH", 'KD', "QS", "AC", "KH", "QC", "AH", 'KD', "QS", "AC", "KH", "QC"];
 const shuffledCards = [];
 let cardLength = cards.length;
 
-while (cards.length > 0 && stopLoop > 0) {
-    let randomList = Math.floor(Math.random() * cardLength);
-    shuffledCards.push(cards.splice(randomList, 1)[0]);
-    // document.querySelectorAll('img')[index].src = `./img/card-pictures/${cards.splice(randomList, 1)}.png`;
-    index++;
-    stopLoop--;
-    cardLength--;
-}
 
-document.querySelectorAll("div.card").forEach((item, index) => { // here
+// document.querySelector('button.start-game').addEventListener('click', function() {
+    startGame = true;
+    while (cards.length > 0 && stopLoop > 0) {
+        let randomList = Math.floor(Math.random() * cardLength);
+        shuffledCards.push(cards.splice(randomList, 1)[0]);
+        index++;
+        stopLoop--;
+        cardLength--;
+    }
+// })
+
+
+document.querySelectorAll("div.card").forEach((item, index) => {
     item.addEventListener('click', e => {
-        console.log(e.target)
-        // document.body.style.backgroundImage = `url("${background_list[index]}")`
+        // if (!startGame) {
+        //     alert("Press start to begin playing");
+        // } else {
+            if (regex.test(e.target.src)) {
+                e.target.src = `./img/card-pictures/${shuffledCards[index]}.png`;
+            } else {
+                console.log(e.target.src)
+                e.target.src = `./img/card-pictures/card-back.png`;
+            }
+        // }
     })
 })
 
