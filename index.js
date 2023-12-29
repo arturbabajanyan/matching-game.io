bc2065932da8baf2f79b86c057bf726a847e745b
arturbabajanyan <arturbabajanyanv@gmail.com>
2023-12-28 22:05:14 -0500

    Game is ready, without styling

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ function reloadThePage() {
     location.reload();
 }
 const regex = new RegExp('card-back');
-const storeTwoCards = {};
+let storedTwoCards = [];
 
 let index = 0;
 let stopLoop = 20;
-let startGame = false;
 
 
 const cards = ["AH", 'KD', "QS", "AC", "KH", "QC", "AH", 'KD', "QS", "AC", "KH", "QC"];
@@ -14,36 +13,42 @@ const shuffledCards = [];
 let cardLength = cards.length;
 
 
-// document.querySelector('button.start-game').addEventListener('click', function() {
-    startGame = true;
-    while (cards.length > 0 && stopLoop > 0) {
-        let randomList = Math.floor(Math.random() * cardLength);
-        shuffledCards.push(cards.splice(randomList, 1)[0]);
-        index++;
-        stopLoop--;
-        cardLength--;
-    }
-// })
+while (cards.length > 0 && stopLoop > 0) {
+    let randomList = Math.floor(Math.random() * cardLength);
+    shuffledCards.push(cards.splice(randomList, 1)[0]);
+    index++;
+    stopLoop--;
+    cardLength--;
+}
 
 
 document.querySelectorAll("div.card").forEach((item, index) => {
     item.addEventListener('click', e => {
-        // if (!startGame) {
-        //     alert("Press start to begin playing");
-        // } else {
-            if (regex.test(e.target.src)) {
-                e.target.src = `./img/card-pictures/${shuffledCards[index]}.png`;
-            } else {
-                console.log(e.target.src)
-                e.target.src = `./img/card-pictures/card-back.png`;
+        if (regex.test(e.target.src)) {
+            e.target.src = `./img/card-pictures/${shuffledCards[index]}.png`;
+            storedTwoCards.push(index, shuffledCards[index]);
+            if (storedTwoCards.length === 4) {
+                setTimeout(checkAnswers, 700);
             }
-        // }
+        } else {
+            e.target.src = `./img/card-pictures/card-back.png`;
+            storedTwoCards = [];
+        }
     })
 })
 
-// for (let i = 0; i < shuffledCards.length; i++) {
-//     console.log(shuffledCards[i]);
-//     document.querySelectorAll('img')[i].src = `./img/card-pictures/${shuffledCards[i]}.png`
-// }
-
-
+function checkAnswers() {
+    console.log(document.querySelectorAll('img')[storedTwoCards[0]]);
+    console.log(document.querySelectorAll('img')[storedTwoCards[2]]);
+    if (storedTwoCards[1] == storedTwoCards[3]) {
+        console.log('You got it');
+        document.querySelectorAll('img')[storedTwoCards[0]].style.display='none';
+        document.querySelectorAll('img')[storedTwoCards[2]].style.display='none';
+    } else {
+        console.log('Wrong answer');
+        document.querySelectorAll('img')[storedTwoCards[0]].src = `./img/card-pictures/card-back.png`;
+        document.querySelectorAll('img')[storedTwoCards[2]].src = `./img/card-pictures/card-back.png`;
+    }
+    storedTwoCards = [];
+    console.log('Done');
+}
